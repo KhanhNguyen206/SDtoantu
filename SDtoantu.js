cfb4354e53977dc67090038b36f655f35baa93f0
KhanhNguyen206 <nguyen.ngockhanh206@gmail.com>
2019-04-24 11:21:37 +0700

    BTSDtoantu js

---
+++ SDtoantu.js
@@ -0,0 +1,10 @@
+let inputWidth;
+inputWidth = prompt("Enter the Width");
+let inputHeight;
+inputHeight = prompt("enter the Height");
+
+let Width = parseInt(inputWidth);
+let Height = parseInt(inputHeight);
+let Area = Width * Height;
+
+document.write("the area is: " + Area);
